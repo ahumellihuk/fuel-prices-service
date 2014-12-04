612ce6588a283377167be2b67ed9a3cf5f9e99bc
Dmitri Samoilov <camoiloc@gmail.com>
2014-12-04 09:29:46 +0000

    Use Sequelize Promise style callback

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,11 @@ var models = require('../models');
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res) {
-  models.station.findAll({include: [ models.pricelist ]}).success(function(stations) {
+  models.station.findAll({include: [ models.pricelist ]}).then(function(stations) {
     res.send(JSON.stringify(stations));
+  }, function() {
+    console.error("Failed to find all stations data.");
   });
 });
 
