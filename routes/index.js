d91b937279a2047704a1cb989ac1f0991ec8870d
Dmitri Samoilov <dmitri@zinosoft.com>
2014-12-10 23:26:42 +0000

    Changed output date format

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var models = require('../models');
 var express = require('express');
 var router = express.Router();
+var moment = require('moment');
+
+Date.prototype.toJSON = function() { return moment(this).format("YYYY-MM-DDTHH:mm:ss ZZ") };
 
 router.get('/', function(req, res) {
   models.station.findAll({include: [ models.pricelist ]}).then(function(stations) {
